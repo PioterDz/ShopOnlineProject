e5a269545cf55b78b1af2ebb18557b1648ffe79f
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-08-27 15:16:12 +0200

    create react app

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.listen(8000, function(){
+  console.log('Server is running on port:', 8000);
+});
